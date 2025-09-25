76748853025f15eb32f272ff89dd8dc53c63269d
Ryan Woods <ryanwoodsmusic@gmail.com>
2025-09-25 10:55:13 -0300

    Removing local scripts for testing.

--- scripts/upload.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# Upload the latest index.html file to neocities
-curl -u "dailybop:mph9myg@ndp6jnw4JZR" -F "index.html=@$HOME/DailyBopProject/index.html" "https://neocities.org/api/upload"
